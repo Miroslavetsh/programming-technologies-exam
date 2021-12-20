7c6a8ee7d7791fca778b98876d6d4381f2e8069d
Miroslavetsh <dfgshte@gmail.com>
2021-12-20 10:48:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const sumOfIntegerSequence = (n = 0) => (n * (n + 1)) / 2
+
+module.exports = sumOfIntegerSequence
---
+++ index.test.js
@@ -0,0 +1,23 @@
+const sumOfIntegerSequence = require('./index')
+
+describe('Test sum of function Xn = 1, 2, 3, 4', () => {
+  test('X() returns number', () => {
+    expect(typeof sumOfIntegerSequence()).toStrictEqual('number')
+  })
+
+  test('X(1) ---> 1 = 1', () => {
+    expect(sumOfIntegerSequence(1)).toStrictEqual(1)
+  })
+
+  test('X(2) ---> 1 + 2 = 3', () => {
+    expect(sumOfIntegerSequence(2)).toStrictEqual(3)
+  })
+
+  test('X(3) ---> 1 + 2 + 3 = 6', () => {
+    expect(sumOfIntegerSequence(3)).toStrictEqual(6)
+  })
+
+  test('X(4) ---> 1 + 2 + 3 + 4 = 6', () => {
+    expect(sumOfIntegerSequence(4)).toStrictEqual(10)
+  })
+})
