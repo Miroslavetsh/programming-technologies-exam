e704a7cf4a801457603a21bbd08aa140eec9309b
Miroslavetsh <dfgshte@gmail.com>
2021-12-20 13:01:24 +0200

    Add extra test cases

--- __tests__/sumOfIntegerSequence.test.js
+++ __tests__/sumOfIntegerSequence.test.js
@@ -1,23 +1,27 @@
-const sumOfIntegerSequence = require('../sumOfIntegerSequence')
+const X = require('../sumOfIntegerSequence')
 
 describe('Test sum of function Xn = 1, 2, 3, 4, ..., n == [sum from 1 to n]', () => {
+  test('X(-12) or X of any non-positive number returns 0', () => {
+    expect(X(-12)).toStrictEqual(0)
+  })
+
   test('X() returns number', () => {
-    expect(typeof sumOfIntegerSequence()).toStrictEqual('number')
+    expect(typeof X()).toStrictEqual('number')
   })
 
   test('X(1) ---> 1 = 1', () => {
-    expect(sumOfIntegerSequence(1)).toStrictEqual(1)
+    expect(X(1)).toStrictEqual(1)
   })
 
   test('X(2) ---> 1 + 2 = 3', () => {
-    expect(sumOfIntegerSequence(2)).toStrictEqual(3)
+    expect(X(2)).toStrictEqual(3)
   })
 
   test('X(3) ---> 1 + 2 + 3 = 6', () => {
-    expect(sumOfIntegerSequence(3)).toStrictEqual(6)
+    expect(X(3)).toStrictEqual(6)
   })
 
   test('X(4) ---> 1 + 2 + 3 + 4 = 6', () => {
-    expect(sumOfIntegerSequence(4)).toStrictEqual(10)
+    expect(X(4)).toStrictEqual(10)
   })
 })
--- sumOfIntegerSequence.js
+++ sumOfIntegerSequence.js
@@ -1,3 +1,6 @@
-const sumOfIntegerSequence = (n = 0) => (n * (n + 1)) / 2
+const sumOfIntegerSequence = (n = 0) => {
+  if (n < 0) return 0
+  return (n * (n + 1)) / 2
+}
 
 if (typeof module !== 'undefined') module.exports = sumOfIntegerSequence
